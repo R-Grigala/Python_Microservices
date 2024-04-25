0bf30395aa2ad72aa8da5a6a681ebddcd54b7214
R-Grigala <r.grigalashvili777@gmail.com>
2024-04-25 14:05:44 +0400

    Coding Continue Add GET And POST

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ProductViewSet
 
 urlpatterns = [
-    path('products/', ProductViewSet.as_view({
+    path('products', ProductViewSet.as_view({
         'get': 'list',
         'post': 'create'
     })),
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,11 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
 
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         pass
