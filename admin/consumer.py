cb3741a87e01084c9e1b99f4849282a5a53cdb7a
R-Grigala <r.grigalashvili777@gmail.com>
2024-04-29 14:01:02 +0400

    Coding Grigala:// Add and Config Docker

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,5 +1,10 @@
 #amqps://mbhkiujr:1U0YbrO4RETrtwul5wK8YHNwBVHzURJq@hawk.rmq.cloudamqp.com/mbhkiujr
-import pika
+import pika, json, os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
 
 params = pika.URLParameters('amqps://mbhkiujr:1U0YbrO4RETrtwul5wK8YHNwBVHzURJq@hawk.rmq.cloudamqp.com/mbhkiujr')
 
@@ -11,7 +16,12 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
     print('Received in admin')
-    print(body)
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased!')
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
